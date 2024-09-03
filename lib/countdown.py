9eecb7e10bdc8683072033dc3fe212c92562e9a0
Nobert-Muma <mumanobert17@gmail.com>
2024-09-03 14:17:30 +0300

    Countdown to Midnight Lab.

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,17 @@
 # your code goes here!
+import time
+def countdown(number):
+    count=number
+    while count>=1:
+        print(f'{count} SECOND(S)!')
+        count-=1
+    print("HAPPY NEW YEAR!")
+def countdown_with_sleep(integer):
+    count=integer
+    while count>=1:
+        print(f"{count} SECOND(S)!")
+        count-=1
+        time.sleep(1)
+    print("HAPPY NEW YEAR!")
+countdown(12)
+countdown_with_sleep(10)
